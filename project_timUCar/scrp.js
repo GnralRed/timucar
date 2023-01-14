ff282a2f342f8de4870ac456095ff99b54a18d87
GnralRed <96296195+GnralRed@users.noreply.github.com>
2023-01-15 04:12:51 +0700

    Add files via upload

---
+++ project_timUCar/scrp.js
@@ -0,0 +1,18 @@
+const burger = document.querySelector(".burger");
+const nav = document.getElementById("nav");
+
+const topBurger = document.querySelector(".burger span:nth-child(1)");
+const centerBurger = document.querySelector(".burger span:nth-child(2)");
+const bottomBurger = document.querySelector(".burger span:nth-child(3)");
+
+window.onscroll = function () {
+  const navbar = document.querySelector("nav");
+  window.pageYOffset > navbar.offsetTop ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+};
+
+burger.addEventListener("click", function () {
+  nav.classList.toggle("right");
+  topBurger.classList.toggle("top-burger");
+  centerBurger.classList.toggle("center-burger");
+  bottomBurger.classList.toggle("bottom-burger");
+});
